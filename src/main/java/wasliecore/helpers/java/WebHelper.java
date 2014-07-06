633d4a9261eb47702f5602eae7707022e94cb392
wasliebob <wasliebob@live.nl>
2014-07-06 19:38:01 +0200

    Huge derp

--- src/main/java/wasliecore/helpers/java/WebHelper.java
+++ src/main/java/wasliecore/helpers/java/WebHelper.java
@@ -4,7 +4,7 @@ import java.awt.Desktop;
 import java.net.URL;
 
 public class WebHelper {
-	public void openURL(String url){
+	public static void openURL(String url){
 		try{
 			Desktop.getDesktop().browse(new URL(url).toURI());
 		}catch(Exception e){
