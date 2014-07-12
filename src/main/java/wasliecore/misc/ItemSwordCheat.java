c9806d4468ad27845f4add92950b14eac134ed85
wasliebob <wasliebob@live.nl>
2014-07-12 21:34:40 +0200

    Added Cheat Sword, dealing an insane amount of damage killing almost all entities

---
+++ src/main/java/wasliecore/misc/ItemSwordCheat.java
@@ -0,0 +1,36 @@
+package wasliecore.misc;
+
+import java.util.List;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.common.util.EnumHelper;
+import wasliecore.main.WaslieCore;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class ItemSwordCheat extends ItemSword{
+
+	public ItemSwordCheat() {
+		super(EnumHelper.addToolMaterial("CHEAT", 4, -1, 10F, 100000000000000000000000000000000000000F, 0));
+		setMaxStackSize(1);
+		setUnlocalizedName(WaslieCore.modName.toLowerCase() + "." + "item" + "." + "cheat sword");
+		setCreativeTab(CreativeTabs.tabCombat);
+		GameRegistry.registerItem(this, this.getUnlocalizedName());
+		setMaxDamage(-1);
+	}
+	
+	@Override
+    public void registerIcons(IIconRegister ir){
+        itemIcon = ir.registerIcon(WaslieCore.modName.toLowerCase() + ":" + "sword_cheat");
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean i){
+		list.add(EnumChatFormatting.RED + "Creative Only");
+	}
+}
--- src/main/java/wasliecore/misc/MiscRegister.java
+++ src/main/java/wasliecore/misc/MiscRegister.java
@@ -7,8 +7,10 @@ public class MiscRegister implements IInitalization{
 	@Override
 	public void preInit() {
 		we = new ItemWE();
+		sword_cheat = new ItemSwordCheat();
 	}
 	public static ItemWE we;
+	public static ItemSwordCheat sword_cheat;
 	
 	@Override
 	public void init() {
